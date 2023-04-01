7c3f0e429576b5b40c4bffcba0eccb7970e1f7fc
kaishajri <rafik_alex53@outlook.fr>
2023-04-01 23:18:49 +0100

    xx

--- src/pages/charge/chef-departement/ConsultationEnseignant.jsx
+++ src/pages/charge/chef-departement/ConsultationEnseignant.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const ConsultationEnseignant = () => {
-    return <div>ConsultationEnseignant</div>
+    return <div>ConsultationEnseignant :C C: O: KAIS +Hamila </div>
 }
 
 export default ConsultationEnseignant
