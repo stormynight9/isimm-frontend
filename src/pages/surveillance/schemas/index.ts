9775aa6a23fc7480c605ce7749b6ad82c1259ea3
stormynight9 <ferjani.nader@hotmail.fr>
2023-04-04 15:11:06 +0100

    feat: added validation to create calendar

--- src/pages/surveillance/schemas/index.ts
+++ src/pages/surveillance/schemas/index.ts
@@ -1,5 +1,8 @@
-import { object, string } from "yup"
+import { number, object, string } from "yup"
 
 export const CreateCalendarSchema = object().shape({
-    title: string().required("titre est obligatoire"),
+    title: string().required("Titre est obligatoire"),
+    numberOfSessions: number().min(1, "Nombre de séances est obligatoire").required("Nombre de séances est obligatoire"),
+    startDate: string().required("Date de début est obligatoire"),
+    endDate: string().required("Date de fin est obligatoire"),
 })
