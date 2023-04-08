6476694603a0d1b1df8fff8e809654c3b89de457
kaishajri <rafik_alex53@outlook.fr>
2023-04-08 23:54:57 +0100

    SubVoeux

---
+++ src/pages/charge/components/ButtonVoeux.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Button } from "@/components/ui/Button"
+const ButtonVoeux = (props) => {
+    return (
+        <Button {...props} variant="default">Voeux</Button>
+    )
+}
+
+export default ButtonVoeux
