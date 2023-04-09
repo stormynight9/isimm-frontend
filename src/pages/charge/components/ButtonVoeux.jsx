578cecb1818b3b592055f9d6f052d02377ac1d20
MaatallahMeriam <mkdmeriam22@gmail.com>
2023-04-10 01:39:59 +0200

    formulaire voeux sans css

--- src/pages/charge/components/ButtonVoeux.jsx
+++ src/pages/charge/components/ButtonVoeux.jsx
@@ -1,8 +1,32 @@
 import React from "react"
 import { Button } from "@/components/ui/Button"
+import FormulaireVoeux from "../enseignant/FormulaireVoeux/FormulaireVoeux"
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/Dialog"
+import { useState } from "react"
+
 const ButtonVoeux = (props) => {
+    const [isVoeuxDialogVisible, setVoeuxDialogVisible] = useState(false)
+
+    const handleClick = () => {
+        setVoeuxDialogVisible(!isVoeuxDialogVisible)
+    }
     return (
-        <Button {...props} variant="default">Voeux</Button>
+        <Dialog className="z-[101]">
+                <DialogTrigger asChild>
+                    <Button {...props} variant="default" onClick={handleClick} >Voeux</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px] ">
+                    <DialogHeader>
+                        <DialogTitle>Demande Voeux :</DialogTitle>
+                        
+                    </DialogHeader>
+                    <FormulaireVoeux/>
+                    <DialogFooter>
+                        <Button type="submit">Valider</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        
     )
 }
 
