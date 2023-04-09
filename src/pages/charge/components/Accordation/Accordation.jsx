e1b9b32ee040d5f6f4e22f3549a33dff4b28f9b7
ahmedhamila <csahmed17@gmail.com>
2023-04-09 12:42:00 +0100

    Css fixes

--- src/pages/charge/components/Accordation/Accordation.jsx
+++ src/pages/charge/components/Accordation/Accordation.jsx
@@ -1,58 +1,59 @@
 import "./Accordation.css"
-import { useState , useEffect } from 'react';
-import { Button } from "@/components/ui/Button";
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/Accordion"
+import { useState, useEffect } from "react"
+import { Button } from "@/components/ui/Button"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/Accordion"
 
 function AccordationCharge() {
-    const [voeux,setVoeux]=useState([])
+    const [voeux, setVoeux] = useState([])
 
     /*Consommation API */
-useEffect(() => {
-  const getVoeux = async () => {
-      const response = await fetch("http://localhost:8090/api/isimm/distributionCharge/enseignantVoeux", {
-          method: "GET",
-          headers: {
-              "Content-Type": "application/json",
-          },
-      })
-      const responseJson = await response.json()
-      console.log(responseJson[0].matiere.name)
-      setVoeux(responseJson)
+    useEffect(() => {
+        const getVoeux = async () => {
+            const response = await fetch("http://localhost:8090/api/isimm/distributionCharge/enseignantVoeux", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            const responseJson = await response.json()
+            console.log(responseJson[0].matiere.name)
+            setVoeux(responseJson)
         }
         getVoeux()
-      }, [])
-   return (
-      <div>
-{ voeux.map((v,i)=>{
-                              return (
-<Accordion type="single" collapsible>
-  <AccordionItem value="item-1">
-    <AccordionTrigger> Voeux d'enseignant  : {v.enseignant.nom} </AccordionTrigger>
-    <AccordionContent>
-      <p>
-      Matiere : {v.matiere.name}
-      </p>
-      <p><br/>
-      Description : {v.voeux.message}
-      </p><br/>
-      <div className="ButtonVoeuxContainner">
-      <Button variant="default" className="ButtonValidVoeux">Valider</Button>
-      <Button variant="default" className="ButtonRejectVoeux">Rejeter</Button>
-      </div>                          
-    </AccordionContent>
-  </AccordionItem>
-</Accordion>
-);})}
-         
-          
-      </div>
-    );
-    
-  }
-  
-  export default AccordationCharge
+    }, [])
+    return (
+        <div className="Accordions">
+            {voeux.map((v, i) => {
+                return (
+                    <Accordion className="Accordion" type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger> Voeux d'enseignant : {v.enseignant.nom} </AccordionTrigger>
+                            <AccordionContent>
+                                <p>
+                                    <span className="bold">Matiere : </span>
+                                    {v.matiere.name}
+                                </p>
+                                <br />
+                                <p>
+                                    <span className="bold">Description : </span>
+                                    {v.voeux.message}
+                                </p>
+                                <br />
+                                <div className="ButtonVoeuxContainner">
+                                    <Button variant="default" className="ButtonValidVoeux">
+                                        Valider
+                                    </Button>
+                                    <Button variant="default" className="ButtonRejectVoeux">
+                                        Rejeter
+                                    </Button>
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                )
+            })}
+        </div>
+    )
+}
+
+export default AccordationCharge
--- src/pages/charge/components/NavBar/NavBar.jsx
+++ src/pages/charge/components/NavBar/NavBar.jsx
@@ -1,22 +1,15 @@
 import { Input } from "@/components/ui/Input"
 import "./NavBar.css"
-import { SearchIcon } from "lucide-react";
-
+import { SearchIcon } from "lucide-react"
 
 function NavBar() {
-
-  return (
-    <nav>
-        <div className='ChargeNavBarContainerStyle'>     
-            <Input type="Text" placeholder="search"  className="ChargeNavBarInputStyle"/>  
-            
-             
-        </div>
-       
-        
-    </nav>
-  );
-  
+    return (
+        <nav>
+            <div className="ChargeNavBarContainerStyle">
+                <Input type="Text" placeholder="search" className="ChargeNavBarInputStyle" />
+            </div>
+        </nav>
+    )
 }
 
 export default NavBar
