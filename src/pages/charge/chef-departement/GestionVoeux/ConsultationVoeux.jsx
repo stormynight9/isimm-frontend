14cfa702312d21d5687fb3ba8317debd7c2f50bd
AsmaGuiza <asma.guiza@ieee.org>
2023-04-09 09:15:41 +0200

    version1InterfaceVoeux

---
+++ src/pages/charge/chef-departement/GestionVoeux/ConsultationVoeux.jsx
@@ -0,0 +1,25 @@
+import React, { useMemo } from "react"
+import NavBar from "@/pages/charge/components/NavBar/NavBar"
+import AccordationCharge from "@/pages/charge/components/Accordation/Accordation"
+import "./ConsultationVoeux.css"
+
+const ConsultationVoeux = () => {
+
+    return(
+        <div className="ConsultationVeuxContainner">
+            <NavBar/>
+            <div className="FrameTitle">
+                <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight"> 
+                    Received Voeux 
+                </h3>
+            </div>
+            <div className="AccordationChargeContainner"> 
+                <AccordationCharge/>
+            </div>
+        </div>
+
+        
+    ) 
+}
+
+export default ConsultationVoeux
---
+++ src/pages/charge/components/Accordation/Accordation.jsx
@@ -0,0 +1,47 @@
+import "./Accordation.css"
+import { useState } from 'react';
+import { Button } from "@/components/ui/Button";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/Accordion"
+const Voeux =[
+    {NomEnseignant:' Hamel Lazhar', NomMatiere:'Analyseur syntaxique' , desc :'Nothing to say'},
+    {NomEnseignant:' Gzara meriem', NomMatiere:'Machine Learning' ,desc : 'Hellllo'}, 
+
+  ]
+function AccordationCharge() {
+    const [voeux,setVoeux]=useState(Voeux)
+
+    return (
+      <div>
+{ Voeux.map((voeux,i)=>{
+                              return (
+<Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger> Voeux d'enseignant {voeux.NomEnseignant} </AccordionTrigger>
+    <AccordionContent>
+      <p>
+      Matiere : {voeux.NomMatiere}
+      </p>
+      <p><br/>
+      Description : {voeux.desc}
+      </p><br/>
+      <div className="ButtonVoeuxContainner">
+      <Button variant="default" className="ButtonValidVoeux">Valider</Button>
+      <Button variant="default" className="ButtonRejectVoeux">Rejeter</Button>
+      </div>                          
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+);})}
+         
+          
+      </div>
+    );
+    
+  }
+  
+  export default AccordationCharge
