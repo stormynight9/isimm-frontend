2e346b924a56fedae531ebcb70866c83f351d4b3
AsmaGuiza <asma.guiza@ieee.org>
2023-04-11 23:51:44 +0200

    merge

--- src/pages/charge/components/Accordation/Accordation.jsx
+++ src/pages/charge/components/Accordation/Accordation.jsx
@@ -22,19 +22,6 @@ function AccordationCharge() {
         getVoeux()
     }, [])
     return (
-<<<<<<< HEAD
-        <div>
-            {voeux.map((v, i) => {
-                return (
-                    <Accordion type="single" collapsible>
-                        <AccordionItem value="item-1">
-                            <AccordionTrigger> Voeux d'enseignant : {v.enseignant.nom} </AccordionTrigger>
-                            <AccordionContent>
-                                <p>Matiere : {v.matiere.name}</p>
-                                <p>
-                                    <br />
-                                    Description : {v.voeux.message}
-=======
         <div className="Accordions">
             {voeux.map((v, i) => {
                 return (
@@ -50,7 +37,6 @@ function AccordationCharge() {
                                 <p>
                                     <span className="bold">Description : </span>
                                     {v.voeux.message}
->>>>>>> 69229e287e296b9665922b85b3455b81b3f72c05
                                 </p>
                                 <br />
                                 <div className="ButtonVoeuxContainner">
