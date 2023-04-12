a14675f0b3924b544eb98e18a23b1c634fb4ec9d
ahmedhamila <csahmed17@gmail.com>
2023-04-12 01:54:38 +0100

    Diplome Pathing

--- src/redux/features/charge/DiplomeApiSlice.js
+++ src/redux/features/charge/DiplomeApiSlice.js
@@ -1,13 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-const URL = "http://localhost:8090/api/isimm/distributionCharge/"
 
 export const DiplomeApi = createApi({
     reducerPath: "DiplomeApi",
-    baseQuery: fetchBaseQuery({ baseUrl: URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
     endpoints: (builder) => ({
         getSemestre: builder.query({
             query: () => ({
-                url: "semestre",
+                url: "/semestre",
                 method: "GET",
             }),
         }),
