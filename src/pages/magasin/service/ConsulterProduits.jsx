b51cc5a4a46f0a5d912fbe7da112a43d9b0556f6
Gharouel Fatma <gharouelfatma@gmail.com>
2023-04-14 00:09:16 +0200

    new pages

--- src/pages/magasin/service/ConsulterProduits.jsx
+++ src/pages/magasin/service/ConsulterProduits.jsx
@@ -1,4 +1,63 @@
+import Table from "@/components/shared/Table"
+import { useMemo } from "react"
+
 const ConsulterProduits = () => {
-    return <div>ConsulterProduits</div>
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Nom de produit",
+                accessor: "NomProduit",
+            },
+            {
+                Header: "Quantité disponible",
+                accessor: "qteDisp",
+            },
+            {
+                Header: "Nom Fournisseurs",
+                accessor: "NomF",
+            },
+            {
+                Header: "Détails",
+                accessor: "détails",
+            },
+        ],
+        []
+    )
+    const data = useMemo(
+        () => [
+            {
+                NomProduit: "câble HDMI",
+                qteDisp: "20",
+                NomF: "InfoTek",
+                détails: "Modifier",
+            },
+            {
+                NomProduit: "câble VGA",
+                qteDisp: "10",
+                NomF: "InfoTek",
+                détails: "Modifier",
+            },
+            {
+                NomProduit: "ramme papiers",
+                qteDisp: "10",
+                NomF: "agrafe",
+                détails: "Modifier",
+            },
+            {
+                NomProduit: "Data show",
+                qteDisp: "05",
+                NomF: "Tunisia net",
+                détails: "Modifier",
+            },
+        ],
+        []
+    )
+    return (
+        <div style={{ margin: "120px" }}>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Consulter produits</h1>
+            <h2 className="mt-10 scroll-m-20 border-b border-b-slate-200 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 dark:border-b-slate-700">Ici, vous pouvez consulter a liste de touts les produits.</h2>
+            <Table columns={columns} data={data} />
+        </div>
+    )
 }
 export default ConsulterProduits
