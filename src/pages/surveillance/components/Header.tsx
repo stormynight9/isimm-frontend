04727d6a7bedb5ff34a2e0c8afaa6db9c7eb66f0
stormynight9 <ferjani.nader@hotmail.fr>
2023-04-17 22:44:36 +0100

    add: added table

--- src/pages/surveillance/components/Header.tsx
+++ src/pages/surveillance/components/Header.tsx
@@ -1,11 +1,12 @@
 interface HeaderProps {
     title: string
     children: React.ReactNode
+    className?: string
 }
 
-const Header = ({ title, children }: HeaderProps) => {
+const Header = ({ title, children, className }: HeaderProps) => {
     return (
-        <header>
+        <header className={className}>
             <h1 className="mb-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{title}</h1>
             <p className="text-slate-700 dark:text-slate-400 lg:text-xl">{children}</p>
         </header>
