80656b3f7a050be4833585d19f79c28e05dd16b9
stormynight9 <ferjani.nader@hotmail.fr>
2023-04-28 11:00:35 +0100

    add: object example

--- src/pages/surveillance/data/saved-calendars.js
+++ src/pages/surveillance/data/saved-calendars.js
@@ -36,3 +36,63 @@ export const savedCalendars = [
         status: false,
     },
 ]
+
+// object example
+const object = {
+    title: "Examen Semestre 2 - 2022/2023",
+    startDate: "21 avril 2023",
+    endDate: "29 avril 2023",
+    numberOfSessions: 4,
+    sessions: [
+        {
+            id: 1,
+            name: "L1-MATH",
+            section: [
+                {
+                    id: 1,
+                    name: "Conception et analyse d’algorithmes",
+                    date: "21 avril 2023",
+                    section: 1,
+                    state: true,
+                },
+                {
+                    id: 2,
+                    name: "Algèbre linéaire",
+                    date: "22 avril 2023",
+                    section: 2,
+                    state: true,
+                },
+                {
+                    id: 3,
+                    name: "Algèbre linéaire",
+                    state: false,
+                },
+            ],
+        },
+        {
+            id: 2,
+            name: "ING1-INFO",
+            section: [
+                {
+                    id: 1,
+                    name: "Frama-C et la preuve de programmes",
+                    date: "21 avril 2023",
+                    section: 3,
+                    state: true,
+                },
+                {
+                    id: 2,
+                    name: "Optimisation combinatoire",
+                    date: "22 avril 2023",
+                    section: 2,
+                    state: true,
+                },
+                {
+                    id: 3,
+                    name: "Algorithmes d’apprentissage automatique",
+                    state: false,
+                },
+            ],
+        },
+    ],
+}
