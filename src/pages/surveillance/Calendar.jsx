c1b185bb3b4a3f465cfa801eeddf7c80eae689fc
stormynight9 <ferjani.nader@hotmail.fr>
2023-04-29 19:07:47 +0100

    add: new ui components

--- src/pages/surveillance/Calendar.jsx
+++ src/pages/surveillance/Calendar.jsx
@@ -3,7 +3,10 @@ import Header from "./components/Header"
 const Calendar = () => {
     return (
         <div className="lg:pt-4 lg:pl-4">
-            <Header title="Calendrier">Voici votre calendrier. Si vous souhaitez envoyer une réclamation, saisissez-la ci-dessous et envoyez-la.</Header>
+            <Header title="Calendrier">
+                Voici votre calendrier. Si vous souhaitez envoyer une réclamation, saisissez-la
+                ci-dessous et envoyez-la.
+            </Header>
         </div>
     )
 }
