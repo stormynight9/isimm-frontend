06f38b4926ed09702d42e35474cdac5e942472f7
Ghazi-Chaftar <89045357+Ghazi-Chaftar@users.noreply.github.com>
2023-04-29 18:04:48 +0200

    petit changement au niveau de url de serveur

--- src/redux/features/surveillance/api/apiSlice.js
+++ src/redux/features/surveillance/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: import.meta.env.VITE_API_URL,
     //   credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = localStorage.getItem("token")
