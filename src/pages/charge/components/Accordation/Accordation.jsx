67224a285a75e2813a364725f91ead5f432700e2
ahmedhamila <csahmed17@gmail.com>
2023-05-05 18:34:48 +0100

    accept voeux

--- src/pages/charge/components/Accordation/Accordation.jsx
+++ src/pages/charge/components/Accordation/Accordation.jsx
@@ -1,7 +1,6 @@
 import "./Accordation.css"
 import { useState, useEffect } from "react"
-import { Button } from "@/components/ui/Button"
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/Accordion"
+import AccordionVoeux from "../AccordionVoeux/AccordionVoeux"
 import { ToastAction } from "@/components/ui/Toast"
 import { useToast } from "@/hooks/useToast"
 
@@ -11,20 +10,7 @@ function AccordationCharge() {
     function showToast(message) {
         showCustomToast(toast, message)
     }
-    /* Handle click */ 
-   const handleClick = async(enseignantId,matiereId ,type) => {
-            console.log(enseignantId)
-            console.log(matiereId)
-            const responseAdd = await fetch(`http://localhost:8090/api/isimm/distributionCharge/enseignantMatiere/updateEnseignantMatiere?matiereId=${matiereId}&enseignantId=${enseignantId}&type=${type}&nombreGroupes=${nbGrpvalue.value}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-            if (responseAdd.ok) {
-                showToast(" Added")
-            }
-    }
+
     /*Consommation API */
     useEffect(() => {
         const getVoeux = async () => {
@@ -44,39 +30,7 @@ function AccordationCharge() {
     return (
         <div className="Accordions">
             {voeux.map((v, i) => {
-                return (
-                    <Accordion className="Accordion" type="single" collapsible key={i}>
-                        <AccordionItem value="item-1">
-                            <AccordionTrigger> Voeux d'enseignant : {v.enseignant.nom + '-'+v.enseignant.prenom} </AccordionTrigger>
-                            <AccordionContent>
-                                <p>
-                                    <span className="bold">Matiere : </span>
-                                    {v.matiere.name}
-                                </p>
-                                <p>
-                                    <span className="bold">Type : </span>
-                                    {v.type}
-                                </p>
-                                <br />
-                                <p>
-                                    <span className="bold">Description : </span>
-                                    {v.voeux.message}
-                                </p>
-                                <br />
-                                <p>
-                                    <span className="bold">Nombre De Groupe : </span>
-                                    {v.matiere.name}
-                                </p>
-                                <div className="ButtonVoeuxContainner">
-                                    <Button variant="default" className="ButtonValidVoeux" onClick={event => handleClick(v.matiere.matiereId,v.enseignant.enseignantId, v.type)}>
-                                        Valider
-                                    </Button>
-
-                                </div>
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                )
+                return <AccordionVoeux key={i} v={v} />
             })}
         </div>
     )
---
+++ src/pages/charge/components/AccordionVoeux/AccordionVoeux.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react"
+import { Button } from "@/components/ui/Button"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/Accordion"
+import Select from "react-select"
+import { ToastAction } from "@/components/ui/Toast"
+import { useToast } from "@/hooks/useToast"
+
+const AccordionVoeux = ({ v }) => {
+    console.log(v)
+    const { toast } = useToast()
+    const [nbGrpvalue, setNbGrpvalue] = useState({ label: "1", value: "1" })
+
+    function showToast(message) {
+        showCustomToast(toast, message)
+    }
+    const handleChangeNbGrp = async (input) => {
+        setNbGrpvalue(input)
+    }
+    /* Handle click */
+    const handleClick = async (enseignantId, matiereId, type) => {
+        console.log(enseignantId)
+        console.log(matiereId)
+        const responseAdd = await fetch(`http://localhost:8090/api/isimm/distributionCharge/enseignantMatiere/updateEnseignantMatiere?matiereId=${matiereId}&enseignantId=${enseignantId}&type=${type}&nombreGroupes=${nbGrpvalue.value}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        if (responseAdd.ok) {
+            showToast(" Added")
+        }
+    }
+    return (
+        <Accordion className="Accordion" type="single" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger> Voeux d'enseignant : {v.enseignant.nom + "-" + v.enseignant.prenom} </AccordionTrigger>
+                <AccordionContent>
+                    <div className="flex flex-row  items-center justify-start  gap-5">
+                        Nombre Groupes :{" "}
+                        <Select
+                            className="SelectEnseignant"
+                            onChange={handleChangeNbGrp}
+                            value={nbGrpvalue}
+                            options={[
+                                { label: "1", value: "1" },
+                                { label: "2", value: "2" },
+                                { label: "3", value: "3" },
+                                { label: "4", value: "4" },
+                            ]}
+                        />
+                    </div>
+                    <p>
+                        <span className="bold">Matiere : </span>
+                        {v.matiere.name}
+                    </p>
+                    <p>
+                        <span className="bold">Type : </span>
+                        {v.type}
+                    </p>
+                    <br />
+                    <p>
+                        <span className="bold">Description : </span>
+                        {v.voeux.message}
+                    </p>
+                    <br />
+
+                    <div className="ButtonVoeuxContainner">
+                        <Button variant="default" className="ButtonValidVoeux" onClick={(event) => handleClick(v.enseignant.enseignantId, v.matiere.matiereId, v.type)}>
+                            Valider
+                        </Button>
+                    </div>
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
+    )
+}
+
+export default AccordionVoeux
+
+export function showCustomToast(toast, message) {
+    toast({
+        title: message,
+        action: <ToastAction altText="Dismiss">D'accord</ToastAction>,
+    })
+}
